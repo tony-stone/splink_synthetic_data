386e2600eb2b2766cdf0e63f724527e36c26411c
Tony Stone <tony-stone@users.noreply.github.com>
2022-10-05 18:32:01 +0100

    parallel shell call

--- parallel_corrupt.sh
+++ parallel_corrupt.sh
@@ -1,6 +1,17 @@
-
 #!/bin/zsh
 set -e
 set -o pipefail
 
-& C:/Users/cm1avs/AppData/Local/pypoetry/Cache/virtualenvs/splink-synthetic-data-wgdmJF1G-py3.10/Scripts/python.exe c:/Users/cm1avs/dev/splink_synthetic_data/07_corrupt_records.py --max_corr_recs=20 --global_prob_mult=1 --set_id=1
+for maxrecs in "50 100"
+do
+    for mult in "1 0.5 2 5"
+    do
+        for id in {1..10}
+        do
+        & C:/Users/cm1avs/AppData/Local/pypoetry/Cache/virtualenvs/splink-synthetic-data-wgdmJF1G-py3.10/Scripts/python.exe c:/Users/cm1avs/dev/splink_synthetic_data/07_corrupt_records.py --max_corr_recs=$maxrecs --global_prob_mult=$mult --set_id=$id
+        done
+    done
+done
+
+echo Finished Successfully
+
