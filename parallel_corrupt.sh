e835cddb10b99effb646f463e3147e34975cdb77
Tony Stone <tony-stone@users.noreply.github.com>
2022-10-19 19:01:26 +0100

    parallel corrupt tidy

--- parallel_corrupt.sh
+++ parallel_corrupt.sh
@@ -8,7 +8,7 @@ do
     do
         for id in {1..10}
         do
-            C:/Users/cm1avs/AppData/Local/pypoetry/Cache/virtualenvs/splink-synthetic-data-wgdmJF1G-py3.10/Scripts/python.exe c:/Users/cm1avs/dev/splink_synthetic_data/07_corrupt_records.py --max_corr_recs=$maxrecs --global_prob_mult=$mult --set_id=$id &
+            poetry run python c:/Users/cm1avs/dev/splink_synthetic_data/07_corrupt_records.py --max_corr_recs=$maxrecs --global_prob_mult=$mult --set_id=$id &
         done
     done
 done
